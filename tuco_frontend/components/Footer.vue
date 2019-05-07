027ad13b6f6ef7f794411871e53079e1158fabc3
Thomas Crone <crone1992@gmail.com>
2019-05-07 16:55:22 +0200

    Added footer for demo of nuxt

---
+++ tuco_frontend/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer_container container-fluid row">
+    <div class="col">
+      <img class="tuco_logo_footer" src="~assets/img/tuco-logo-400.png" alt="tuco_logo">
+    </div>
+    <div class="col">
+      <h4 class="footer_headline">Sitemap</h4>
+    </div>
+    <div class="col">
+      <h4 class="footer_headline">Information</h4>
+    </div>
+    <div class="col">
+      <h4 class="footer_headline">Contact</h4>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
--- tuco_frontend/pages/index.vue
+++ tuco_frontend/pages/index.vue
@@ -1,13 +1,6 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
-      <h1 class="title text-danger">tuco_frontend</h1>
-      <h2 class="subtitle">frontend for tuco website</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
       <div v-if="references">
         Reference name from Strapi:
         <b>{{references[0].ref_name}}</b>
@@ -17,7 +10,6 @@
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
 import Strapi from "strapi-sdk-javascript";
 const strapi = new Strapi("http://localhost:1337");
 
@@ -28,9 +20,7 @@ export default {
       references: null
     };
   },
-  components: {
-    Logo
-  },
+  components: {},
   created() {
     this.fetchData();
     console.log(this.references);
@@ -50,34 +40,4 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
