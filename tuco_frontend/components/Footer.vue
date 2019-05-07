fdb214f7f98aa181fa1745bd8cccfc80d49c186e
Mathias Jacobsen <36068677+Mathias95@users.noreply.github.com>
2019-05-08 00:02:10 +0200

    btn global + footer styling

--- tuco_frontend/components/Footer.vue
+++ tuco_frontend/components/Footer.vue
@@ -1,17 +1,17 @@
 <template>
   <footer class="footer_container">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-3 follow_us">
+    <b-container>
+      <b-row>
+        <b-col cols="12" md="6" lg="6" class="follow_us">
           <a class="footer_logo" href="#"><img src="~assets/img/tuco_logo.png" alt="Tuco Logo"></a>
           <h4>Follow Us</h4>
-          <ul class="d-flex">
-            <li><a href="">Facebook</a></li>
-            <li><a href="">YouTube</a></li>
-            <li><a href="">Linkedin</a></li>
+          <ul class="social_icons">
+            <li class="social_icon"><a href="">icon</a></li>
+            <li class="social_icon"><a href="">icon</a></li>
+            <li class="social_icon"><a href="">icon</a></li>
           </ul>
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-3">
+        </b-col>
+        <b-col cols="12" md="6" lg="2">
           <h4 class="footer_headline">Sitemap</h4>
           <ul>
             <li><a href="#">Welcome</a></li>
@@ -21,8 +21,8 @@
             <li><a href="#">News</a></li>
             <li><a href="#">About Tuco</a></li>
           </ul>
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-3">
+        </b-col>
+        <b-col cols="12" md="6" lg="2">
           <h4 class="footer_headline">Information</h4>
           <ul class="bottom-0">
             <li>Lorem Ipsum</li>
@@ -30,16 +30,16 @@
             <li>Lorem Ipsum</li>
             <li>Lorem Ipsum</li>
           </ul>
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-3">
+        </b-col>
+        <b-col cols="12" md="6" lg="2">
           <h4 class="footer_headline">Contact</h4>
           <ul class="bottom-0">
             <li>Krogsbjergvej 2 <br> DK-5600 Faaborg</li>
-            <li>+45 7020 3898</li>
-            <li>mail@tuco.dk</li>
+            <li><a href="tel:+4570203898">+45 7020 3898</a></li>
+            <li><a href="mailto:someone@example.com?Subject=Hello%20again">mail@tuco.dk</a></li>
           </ul>
-        </div>
-      </div>
-    </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </footer>
 </template>
