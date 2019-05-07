925789c6996eed539f394020e8faf6dd101cfa64
Mathias Jacobsen <36068677+Mathias95@users.noreply.github.com>
2019-05-08 01:14:24 +0200

    fixing headed - still has a smal problemstyling

--- tuco_frontend/components/Footer.vue
+++ tuco_frontend/components/Footer.vue
@@ -2,8 +2,8 @@
   <footer class="footer_container">
     <b-container>
       <b-row>
-        <b-col cols="12" md="6" lg="6" class="follow_us">
-          <a class="footer_logo" href="#"><img src="~assets/img/tuco_logo.png" alt="Tuco Logo"></a>
+        <b-col cols="12" md="6" lg="3" xl="6" class="follow_us">
+          <a class="footer_logo" href="/"><img src="~assets/img/tuco_logo.png" alt="Tuco Logo"></a>
           <h4>Follow Us</h4>
           <ul class="social_icons">
             <li class="social_icon"><a href="">icon</a></li>
@@ -11,7 +11,7 @@
             <li class="social_icon"><a href="">icon</a></li>
           </ul>
         </b-col>
-        <b-col cols="12" md="6" lg="2">
+        <b-col cols="12" md="6" lg="3" xl="2">
           <h4 class="footer_headline">Sitemap</h4>
           <ul>
             <li><a href="#">Welcome</a></li>
@@ -22,7 +22,7 @@
             <li><a href="#">About Tuco</a></li>
           </ul>
         </b-col>
-        <b-col cols="12" md="6" lg="2">
+        <b-col cols="12" md="6" lg="3" xl="2">
           <h4 class="footer_headline">Information</h4>
           <ul class="bottom-0">
             <li>Lorem Ipsum</li>
@@ -31,7 +31,7 @@
             <li>Lorem Ipsum</li>
           </ul>
         </b-col>
-        <b-col cols="12" md="6" lg="2">
+        <b-col cols="12" md="6" lg="3" xl="2">
           <h4 class="footer_headline">Contact</h4>
           <ul class="bottom-0">
             <li>Krogsbjergvej 2 <br> DK-5600 Faaborg</li>
--- tuco_frontend/components/Header.vue
+++ tuco_frontend/components/Header.vue
@@ -1,33 +1,42 @@
 <template>
-  <header class="header_container container-fluid">
-    <div class="row top_bar">
-      <div class="col">Phone</div>
-      <div class="col">Email</div>
-    </div>
-    <div class="row tuco_navbar">
-      <div class="col">
-        <!-- Trying to use https://bootstrap-vue.js.org/docs/components/navbar/ -->
-        <b-navbar toggleable="lg" type="dark">
-          <b-navbar-brand href="#">
-            <img class="header_logo" src="~assets/img/tuco_logo.png" alt="Tuco Logo">
-          </b-navbar-brand>
-
-          <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
-
-          <b-collapse id="nav-text-collapse" is-nav>
-            <b-navbar-nav class="ml-auto">
-              <b-nav-item :to="{path: '/#'}">Home</b-nav-item>
-              <b-nav-item :to="{path: '/carbon'}">Why Carbon?</b-nav-item>
-              <b-nav-item :to="{path: '/references'}">References</b-nav-item>
-              <b-nav-item :to="{path: '/cases'}">Cases</b-nav-item>
-              <b-nav-item :to="{path: '/news'}">News</b-nav-item>
-              <b-nav-item :to="{path: '/about'}">About Tuco</b-nav-item>
-              <b-nav-item :to="{path: '/contact'}" class="get_in_touch_btn">Get in touch</b-nav-item>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-navbar>
+  <div>
+    <header>
+      <div class="container-fluid top_bar">
+        <div class="row">
+          <div class="col">
+            <div class="tuco_navbar">
+              <ul class="header-contact">
+                <li>+45 7020 3898</li>
+                <li>mail@tuco.dk</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
+    <nav>
+        <div class="tuco_navbar">
+            <b-navbar toggleable="lg" type="dark">
+            <b-navbar-brand href="/#">
+              <img class="header_logo" src="~assets/img/tuco_logo.png" alt="Tuco Logo">
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-text-collapse" is-nav>
+              <b-navbar-nav class="ml-auto menu-items">
+                <b-nav-item :to="{path: '/#'}">Home</b-nav-item>
+                <b-nav-item :to="{path: '/carbon'}">Why Carbon?</b-nav-item>
+                <b-nav-item :to="{path: '/references'}">References</b-nav-item>
+                <b-nav-item :to="{path: '/cases'}">Cases</b-nav-item>
+                <b-nav-item :to="{path: '/news'}">News</b-nav-item>
+                <b-nav-item :to="{path: '/about'}">About Tuco</b-nav-item>
+                <b-nav-item :to="{path: '/contact'}" class="btn btn-main">Get in touch</b-nav-item>
+              </b-navbar-nav>
+            </b-collapse>
+          </b-navbar>
+        </div>
+    </nav>
+  </div>
 </template>
         
