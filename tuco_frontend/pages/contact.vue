06fc5479878bc403a4894ad69f65826ce18b50da
Pratheep <theep1891@gmail.com>
2019-05-07 20:10:52 +0200

    Added contact page

---
+++ tuco_frontend/pages/contact.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class id="contact">
+    <div class>
+      <div class="contact-box col-md-8">
+        <div class="p-md-5">
+           <h2 class="boldtext h3">Kontakt
+            <span class="colorgreentext">information</span>
+          </h2>
+          <p>
+            Tuco Marine ApS <br>
+Krogsbjergvej 2 <br>
+DK-5600 Faaborg.
+            <br>
+          </p>
+          <p>
+            <a class="boldtext" href="tel:+4570203898">Tlf. +45 7020 3898</a>
+            <br>
+            <a class="boldtext" href="mailto:Lvsinfo@lavs.dk">mail@tuco.dk</a>
+            <br>EAN-nr. xxxxxxxxxxx
+            <br>Cvr.nr. xxxxxxx
+          </p>
+          <p>
+            <strong class="colorgreentext">Åbningstider:</strong>
+            <br>Alle dage fra. 08.00 – 16.00
+            <br>Undtagen Lørdage og Søndage (Lukket)
+          </p>
+        </div>
+      </div>
+     <iframe class="bb col-md pr-0" frameborder="0" allowfullscreen
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9128.103665226527!2d10.218865!3d55.112819!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x113b659355105862!2sTuco+Yacht+Shipyard+Ltd.!5e0!3m2!1sen!2sus!4v1550659258534" width="600" height="450" style="border:0">
+      </iframe>
+    </div>
+    <div class="container employees-filter">
+      <div class="row py-5">
+        <div class="col-md-6 m-auto text-center">
+          <h1 class="boldtext h2 mt-4">THE PEOPLE BEHIND
+            <span class="colorgreentext">TUCO</span>
+          </h1>
+        </div>
+        <div class="emp-filtering mt-4 col-12 d-flex jc-center">
+          <div class="filter pb-3">
+            <a class="active" href="#all">Alle</a>
+            <a href="#Bestyrelse" class="Bestyrelse">Bestyrelsen</a>
+            <a href="#admin-commu" class="admin-commu">Administration og Kommunikation</a>
+            <a href="#management" class="management">Ledelse</a>
+            <a href="#it" class="press-contact">IT</a>
+            <a href="#subject-worker" class="subject-worker">Fagmedarbejdere</a>
+          </div>
+        </div>
+      </div>
+      <div class="employee-content row sets mb-5">
+        <div class="col-md-3">
+          <div class="employee-item Bestyrelse management">
+             <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
+            <div class="p-3">
+              <h2>Jonas Pedersen</h2>
+              <p>
+                <strong>Director</strong>
+              </p>
+              <a href="tel:xxxxxx">Mobil: xx xx xx xx</a><br>
+              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="employee-item Bestyrelse management">
+            <!-- <div
+              style="background-image: url('http://new.ecocouncil.dk/media/com_reditem/images/customfield/item/50/thumbs/800x800/060471a84a11b288711b173ec3d5235f6b28ebdc.jpg')"
+              class="img"></div> -->
+                <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
+            <div class="p-3">
+              <h2>Jonas Pedersen</h2>
+              <p>
+                <strong>Director</strong>
+              </p>
+              <a href="tel:xxxxxx">Mobil: xx xx xx xx</a><br>
+              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="employee-item Bestyrelse management">
+            <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
+            <div class="p-3">
+              <h2>Jonas Pedersen</h2>
+              <p>
+                <strong>Director</strong>
+              </p>
+              <a href="tel:xxxxxx">Mobil: xx xx xx xx</a><br>
+              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="employee-item Bestyrelse management">
+            <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
+            <div class="p-3">
+               <h2>Jonas Pedersen</h2>
+              <p>
+                <strong>Director</strong>
+              </p>
+              <a href="tel:xxxxxx">Mobil: xx xx xx </a><br>
+              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
+            </div>
+          </div>
+        </div>
+         <div class="col-md-3">
+          <div class="employee-item Bestyrelse management">
+            <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
+            <div class="p-3">
+               <h2>Jonas Pedersen</h2>
+              <p>
+                <strong>Director</strong>
+              </p>
+              <a href="tel:xxxxxx">Mobil: xx xx xx </a><br>
+              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
+            </div>
+          </div>
+        </div>
+         <div class="col-md-3">
+          <div class="employee-item Bestyrelse management">
+            <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
+            <div class="p-3">
+               <h2>Jonas Pedersen</h2>
+              <p>
+                <strong>Director</strong>
+              </p>
+              <a href="tel:xxxxxx">Mobil: xx xx xx </a><br>
+              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
+            </div>
+          </div>
+        </div>
+         <div class="col-md-3">
+          <div class="employee-item Bestyrelse management">
+            <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
+            <div class="p-3">
+               <h2>Jonas Pedersen</h2>
+              <p>
+                <strong>Director</strong>
+              </p>
+              <a href="tel:xxxxxx">Mobil: xx xx xx </a><br>
+              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
+            </div>
+          </div>
+        </div>
+         <div class="col-md-3">
+          <div class="employee-item Bestyrelse management">
+            <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
+            <div class="p-3">
+               <h2>Jonas Pedersen</h2>
+              <p>
+                <strong>Director</strong>
+              </p>
+              <a href="tel:xxxxxx">Mobil: xx xx xx </a><br>
+              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Contact'
+}
+//   ('.employees-filter .filter a').click(function(e) {
+//     e.preventDefault ()
+//     var a = (this).attr('href')
+//      a = a.substr (1)
+//      ('.sets .employee-item').each(function () {
+//      if (!(this).hasClass(a) && a !== 'all')
+//        (this).addClass('hide')
+//      else (this).removeClass('hide')
+//    })
+//  })
+//  ( ".filter a" ).on('click', function() {
+//       $(".filter a").removeClass('active')
+//       $(this).addClass('active')
+//     })
+</script>
