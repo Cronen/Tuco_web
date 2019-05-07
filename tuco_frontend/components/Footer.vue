a7e1f80ec7aa2a4ff7d44fc92008ab675cb2b5dd
Mathias Jacobsen <36068677+Mathias95@users.noreply.github.com>
2019-05-07 19:12:32 +0200

    added changes to footer

--- tuco_frontend/components/Footer.vue
+++ tuco_frontend/components/Footer.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="footer_container container-fluid row">
-    <div class="col">
-      <img class="tuco_logo_footer" src="~assets/img/tuco-logo-400.png" alt="tuco_logo">
+  <footer class="footer_container">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <img src="~assets/img/tuco-logo-400.png" alt="Tuco Logo">
+        </div>
+        <div class="col">
+          <h4 class="footer_headline">Sitemap</h4>
+        </div>
+        <div class="col">
+          <h4 class="footer_headline">Information</h4>
+        </div>
+        <div class="col">
+          <h4 class="footer_headline">Contact</h4>
+        </div>
+      </div>
     </div>
-    <div class="col">
-      <h4 class="footer_headline">Sitemap</h4>
-    </div>
-    <div class="col">
-      <h4 class="footer_headline">Information</h4>
-    </div>
-    <div class="col">
-      <h4 class="footer_headline">Contact</h4>
-    </div>
-  </div>
+  </footer>
 </template>
 
 <style lang="scss">
