1414a66654ad4549fe0dbbd0b0cdfcec943d0639
Mathias Jacobsen <36068677+Mathias95@users.noreply.github.com>
2019-05-07 21:28:16 +0200

    footer responsive

--- tuco_frontend/components/Footer.vue
+++ tuco_frontend/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer_container">
     <div class="container">
       <div class="row">
-        <div class="col follow_us">
+        <div class="col-sm-12 col-md-6 col-lg-3 follow_us">
           <a class="footer_logo" href="#"><img src="~assets/img/tuco_logo.png" alt="Tuco Logo"></a>
           <h4>Follow Us</h4>
           <ul class="d-flex">
@@ -11,7 +11,7 @@
             <li><a href="">Linkedin</a></li>
           </ul>
         </div>
-        <div class="col">
+        <div class="col-sm-12 col-md-6 col-lg-3">
           <h4 class="footer_headline">Sitemap</h4>
           <ul>
             <li><a href="#">Welcome</a></li>
@@ -22,13 +22,19 @@
             <li><a href="#">About Tuco</a></li>
           </ul>
         </div>
-        <div class="col">
+        <div class="col-sm-12 col-md-6 col-lg-3">
           <h4 class="footer_headline">Information</h4>
+          <ul class="bottom-0">
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
+          </ul>
         </div>
-        <div class="col">
+        <div class="col-sm-12 col-md-6 col-lg-3">
           <h4 class="footer_headline">Contact</h4>
-          <ul>
-            <li>Krogsbjergvej 2 DK-5600 Faaborg</li>
+          <ul class="bottom-0">
+            <li>Krogsbjergvej 2 <br> DK-5600 Faaborg</li>
             <li>+45 7020 3898</li>
             <li>mail@tuco.dk</li>
           </ul>
