4f01f7263d51633ea919a5062511f0875d32ec04
Mathias Jacobsen <36068677+Mathias95@users.noreply.github.com>
2019-05-07 19:47:51 +0200

    added scss structure

--- tuco_frontend/components/Footer.vue
+++ tuco_frontend/components/Footer.vue
@@ -3,16 +3,35 @@
     <div class="container">
       <div class="row">
         <div class="col">
-          <img src="~assets/img/tuco-logo-400.png" alt="Tuco Logo">
+          <a href="#"><img src="~assets/img/tuco-logo-400.png" alt="Tuco Logo"></a>
+          <h4>Follow Us</h4>
+          <ul class="d-flex">
+            <li><a href="">Facebook</a></li>
+            <li><a href="">YouTube</a></li>
+            <li><a href="">Linkedin</a></li>
+          </ul>
         </div>
         <div class="col">
           <h4 class="footer_headline">Sitemap</h4>
+          <ul>
+            <li><a href="#">Welcome</a></li>
+            <li><a href="#">Why Carbon?</a></li>
+            <li><a href="#">References</a></li>
+            <li><a href="#">Cases</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">About Tuco</a></li>
+          </ul>
         </div>
         <div class="col">
           <h4 class="footer_headline">Information</h4>
         </div>
         <div class="col">
           <h4 class="footer_headline">Contact</h4>
+          <ul>
+            <li>Krogsbjergvej 2 DK-5600 Faaborg</li>
+            <li>+45 7020 3898</li>
+            <li>mail@tuco.dk</li>
+          </ul>
         </div>
       </div>
     </div>
