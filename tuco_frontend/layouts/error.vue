e04da01ffc00597823548717a889bafd44071bc6
Thomas Crone <crone1992@gmail.com>
2019-05-08 10:59:30 +0200

    Updated 404 page

--- tuco_frontend/layouts/error.vue
+++ tuco_frontend/layouts/error.vue
@@ -1,9 +1,9 @@
 <!-- 404 error page -->
 <template>
-  <div class="container">
+  <div class="row justify-content-center align-items-center p-5">
+    <img src="~assets/img/error-404-boat.png" alt="404 image"/>
     <h1 v-if="error.statusCode === 404">Page not found</h1>
     <h1 v-else>An error occurred</h1>
-    <nuxt-link to="/">Back</nuxt-link>
   </div>
 </template>
 <script>
