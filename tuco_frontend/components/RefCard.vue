e40e452333b8bfcda78579dfaf3122ab0a2fc427
Thomas Crone <crone1992@gmail.com>
2019-05-09 18:18:19 +0200

    Made update to references with ref-component and data from Strapi

---
+++ tuco_frontend/components/RefCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class>
+    <a :href="this.reference.ref_url">
+      <img class="radius_box" :src="ref_img" :alt="this.reference.ref_name">
+    </a>
+  </article>
+</template>
+
+<script>
+const strapi_url = process.env.StrapiUrl;
+
+export default {
+  data() {
+    return {
+      ref_img: strapi_url + this.reference.ref_logo.url
+    };
+  },
+  props: {
+    reference: Object
+  },
+  created() {
+    console.log(this.reference);
+  }
+};
+</script>
+
+<style>
+</style>
--- tuco_frontend/pages/references.vue
+++ tuco_frontend/pages/references.vue
@@ -9,8 +9,12 @@
           </b-col>
         </b-row>
       </b-container>
-      <b-container>
+      <b-container v-if="references">
         <b-row>
+           <b-col cols="6" sm="4" md="3" lg="2" v-for="r in references" v-bind:key="r.id">
+            <RefCard :reference="r"/>
+           </b-col>
+           <!-- TODO: remove below when testing is done -->
           <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
           <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
           <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
@@ -20,17 +24,52 @@
           <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
           <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
           <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
-          <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
-          <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
-          <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
-          <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
-          <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
-          <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
-          <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
-          <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
-          <b-col cols="6" sm="4" md="3" lg="2"><a href="#"><img class="radius_box" src="../assets/img/references/Danskemaritime.jpg" alt=""></a></b-col>
+          <!-- Till here -->
+        </b-row>
+      </b-container>
+      <b-container v-else>
+        <b-row>
+          <b-col cols="1" class="mx-auto">
+            <Loader/>         
+          </b-col>
         </b-row>
       </b-container>
     </section>
   </div>
 </template>
+
+<script>
+import Loader from '~/components/Loader.vue'
+import RefCard from '~/components/RefCard.vue'
+import Strapi from "strapi-sdk-javascript";
+const strapi_url = process.env.StrapiUrl;
+const strapi = new Strapi(strapi_url);
+
+export default {
+  data() {
+    return {
+      loading: false,
+      references: null,
+      strapi_url: strapi_url
+    };
+  },
+  components: {
+    Loader,
+    RefCard
+  },
+  created() {
+    this.fetchData();
+    console.log(this.references);
+  },
+  methods: {
+    async fetchData() {
+      this.loading = true;
+      let response = await strapi
+        .request("get", "/references")
+        .then((this.loading = false));
+      console.log(response[0]);
+      this.references = response;
+    }
+  }
+};
+</script>
