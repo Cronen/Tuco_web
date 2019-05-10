6920a8acc168368bb4b1697e7b5af4836927145b
Mathias Jacobsen <36068677+Mathias95@users.noreply.github.com>
2019-05-10 18:57:20 +0200

    newsletter signup bg needs on mobile

--- tuco_frontend/components/FrontBanner.vue
+++ tuco_frontend/components/FrontBanner.vue
@@ -6,8 +6,8 @@
                 <b-row>
                     <b-col cols="12" md="9" lg="5">
                         <h1 class="banner_title">Fast boats for professionals</h1>
-                        <p class="banner_subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
-                        <a class="btn btn-main" href="">Read more at Prozero</a>
+                        <p class="banner_subtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+                        <a class="btn btn-main" href="#">Read more at Prozero</a>
                     </b-col>
                 </b-row>
             </b-container>
--- tuco_frontend/components/NewsletterForm.vue
+++ tuco_frontend/components/NewsletterForm.vue
@@ -1,19 +1,22 @@
 <template>
-    <section class="section_padding">
-      <b-container>
-        <b-row>
-          <b-col class="text-center">
-            <h4>Join Tuco Marines Newsletter</h4>
-            <p>Subscribe now and get the latest boat news</p>
-            <div class="input_wrapper">
-              <form class="signup_form" action="">
-                <input class="signup_input" type="email" name="email" placeholder="Enter your email adress">
-                <button type="submit"><font-awesome-icon :icon="['fas', 'long-arrow-alt-right']"/></button>
-              </form> 
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
+    <section class="signup_bg">
+      <img src="~assets/img/signup-bg.jpg" alt="">
+      <div class="newsform_wrapper">
+        <b-container>
+          <b-row>
+            <b-col class="text-center">
+              <h4 class="signup_title">Join Tuco Marines Newsletter</h4>
+              <p class="signup_subtext">Subscribe now and get the latest boat news</p>
+              <div class="input_wrapper radius_box">
+                <form class="signup_form" action="">
+                  <input class="signup_input" type="email" name="email" placeholder="Enter your email adress">
+                  <button class="signup_btn" type="submit"><font-awesome-icon :icon="['fas', 'long-arrow-alt-right']"/></button>
+                </form> 
+              </div>
+            </b-col>
+          </b-row>
+        </b-container>
+      </div>
     </section>
 </template>
 
