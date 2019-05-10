b684aeb37f68ebada391690d4c0147f24cb18d9d
Pratheep <theep1891@gmail.com>
2019-05-10 11:36:47 +0200

    Quality page markup updated

---
+++ tuco_frontend/pages/quality.vue
@@ -0,0 +1,70 @@
+<template>
+<div>
+    <section-1 id="quality" class="hero">
+        <!-- <div>
+        <img src="../assets/images/quality3.jpg">
+      </div> -->
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7">
+            <h1 class="pb-3 color-white wow fadeIn delay-1">
+              Quality and Safety
+              <span class="boldtext colorgreentext">are Fundamental keywords </span> at Tuco Marine Group.
+            </h1>
+            <p class="wow fadeIn delay-2">"When working with composite materials it is crucial that the quality is 100 percent under control. Our dedicated and skilled employees ensure the high quality in every step of the manufacturing process – right from the selection of materials and quality control when entering the facilities at Tuco to the final approval of the finished product. "</p>
+           </div>
+        </div>
+      </div>
+    </section-1>
+     <section class="my-5 py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5">
+            <h1 class="h3 pb-3">
+              <b>Control & Documentation</b>
+            </h1>
+            <p class
+            >At Tuco we perform our quality control by running tests and samples of our products and materials on a regular basis through the whole production process. Besides testing the tensile strength and elasticity the hardening values of each molding is measured in order to ensure our high quality.
+              <br>
+              <br>For each individual project Tuco makes quality papers. Documentation, quality procedures and manuals follow the manufacturing process. The quality papers are specified down to each single subject, gathering, assemblage, molding and surface treatment. Each subject and part is given it’s own number to obtain the highest possible level of traceability.  The operator performing the task fills out the papers and at the same time every step is supervised and signed for by the quality inspector assigned to the project. The quality inspector is responsible for the project.
+            </p>
+          </div>
+          <div class="col-md-6 video-col">
+            <img src="../assets/img/quality1.jpg">&nbsp;
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="my-5 py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 video-col">
+            <img src="../assets/img/quality2.jpg">&nbsp;
+         </div>
+          <div class="col-md-5">
+            <h1 class="h3 pb-3"><b>Classification Approved Projects</b>
+            </h1>
+            <p
+              class
+            >Tuco has many years of experience with manufacturing vessels approved by different classification companies and we have collaborated with companies like Lloyds and DNV to their full satisfaction.
+              <br>
+              <br>When working on class approved projects – besides Tuco’s own quality control – there are frequent visits by specialist quality inspectors from the classification company in question. Tuco has a close and good cooperation with the classification company on regularly optimizing the quality- and production documentation.
+             </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div style="text-align:center;" >
+        <i><b>At Tuco we are ready to find the best possible solution that fully meets with your needs and demands while living up to and maintaining the high quality level set by both Tuco and the classification companies.</b></i>
+      </div>
+       <br>
+       <div style="text-align:center;">
+           <b>Give us a call at +45 7020 3898.</b>
+       </div>
+       <br> <br>
+     </div>
+    </section>
+</div>
+</template>
