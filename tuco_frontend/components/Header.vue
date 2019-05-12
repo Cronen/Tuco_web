b4120518760b9987915002ab871d74daee890c1f
Mathias Jacobsen <36068677+Mathias95@users.noreply.github.com>
2019-05-12 13:45:45 +0200

    fixed navbar position

--- tuco_frontend/components/Header.vue
+++ tuco_frontend/components/Header.vue
@@ -1,37 +1,39 @@
 <template>
   <div>
-    <header>
-      <div class="container-fluid top_bar">
-        <div class="row">
-          <div class="col">
-            <ul class="header-contact">
-              <li><font-awesome-icon :icon="['fas', 'mobile-alt']"/><a href="tel:+4570203898">+45 7020 3898</a></li>
-              <li><font-awesome-icon :icon="['fas', 'envelope']"/><a href="mailto:someone@example.com?Subject=Hello%20again">mail@tuco.dk</a></li>
-            </ul>
+    <div class="navbar-wrapper">
+      <header>
+        <div class="container-fluid top_bar">
+          <div class="row">
+            <div class="col">
+              <ul class="header-contact">
+                <li><font-awesome-icon :icon="['fas', 'mobile-alt']"/><a href="tel:+4570203898">+45 7020 3898</a></li>
+                <li><font-awesome-icon :icon="['fas', 'envelope']"/><a href="mailto:someone@example.com?Subject=Hello%20again">mail@tuco.dk</a></li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
-        <b-navbar toggleable="lg" type="dark" class="main_bar">
-          <b-navbar-brand href="/">
-            <img class="header_logo" src="~assets/img/tuco_logo.png" alt="Tuco Logo">
-          </b-navbar-brand>
+      </header>
+      <b-navbar toggleable="lg" type="dark" class="main_bar">
+        <b-navbar-brand href="/">
+          <img class="header_logo" src="~assets/img/tuco_logo.png" alt="Tuco Logo">
+        </b-navbar-brand>
 
-          <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
 
-          <b-collapse id="nav-text-collapse" is-nav>
-            <b-navbar-nav class="ml-auto menu-items">
-              <b-nav-item :to="{path: '/'}">Home</b-nav-item>
-              <b-nav-item :to="{path: '/whycarbon'}">Why Carbon?</b-nav-item>
-              <!--<b-nav-item :to="{path: '/quality'}">QUALITY</b-nav-item>-->
-              <b-nav-item :to="{path: '/references'}">References</b-nav-item>
-              <b-nav-item :to="{path: '/cases'}">Cases</b-nav-item>
-              <b-nav-item :to="{path: '/news'}">News</b-nav-item>
-              <b-nav-item :to="{path: '/about'}">About Tuco</b-nav-item>
-              <b-nav-item :to="{path: '/contact'}" class="btn btn-main">Get in touch</b-nav-item>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-navbar>
+        <b-collapse id="nav-text-collapse" is-nav>
+          <b-navbar-nav class="ml-auto menu-items">
+            <b-nav-item :to="{path: '/'}">Home</b-nav-item>
+            <b-nav-item :to="{path: '/whycarbon'}">Why Carbon?</b-nav-item>
+            <!--<b-nav-item :to="{path: '/quality'}">QUALITY</b-nav-item>-->
+            <b-nav-item :to="{path: '/references'}">References</b-nav-item>
+            <b-nav-item :to="{path: '/cases'}">Cases</b-nav-item>
+            <b-nav-item :to="{path: '/news'}">News</b-nav-item>
+            <b-nav-item :to="{path: '/about'}">About Tuco</b-nav-item>
+            <b-nav-item :to="{path: '/contact'}" class="btn btn-main">Get in touch</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
   </div>
 </template>
         
