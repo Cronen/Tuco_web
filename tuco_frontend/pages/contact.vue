bbdec82ceefeda1d078ed11504ffa2af3450f955
Mathias Jacobsen <36068677+Mathias95@users.noreply.github.com>
2019-05-13 13:47:31 +0200

    map styled

--- tuco_frontend/pages/contact.vue
+++ tuco_frontend/pages/contact.vue
@@ -36,13 +36,35 @@ DK-5600 Faaborg.
     <subbanner/>
     <section class="section_padding">
       <b-container>
-        <b-row>
-          <b-col>
-            <div class="map_location">
+        <b-row class="relative">
+          <b-col cols="12" class="location_info">
+              <div class="location_address">
+                <h2 class="adress_title">Tuco - Krogsbjergvej 2</h2>
+                <ul>
+                  <li>5600 Faaborg</li>
+                  <li>Denmark</li>
+                </ul>
+              </div>
+              <div class="location_phone">
+                <h4>Phone number</h4>
+                <a href="tel:+4570203898">+45 7020 3898</a>
+              </div>
+              <div class="location_mail">
+                <h4>Email</h4>
+                <a href="mailto:someone@example.com?Subject=Hello%20again">mail@tuco.dk</a>
+              </div>
+              <div class="location_media">
+                <ul>
+                  <li class="location_media_icon"><a target="_blank" href="https://www.facebook.com/pages/Tuco-Marine-Group-ApS/250239778331751?fref=ts"><font-awesome-icon :icon="['fab', 'facebook-f']"/></a></li>
+                  <li class="location_media_icon"><a target="_blank" href="http://www.youtube.com/tucomarine"><font-awesome-icon :icon="['fab', 'youtube']"/></a></li>
+                  <li class="location_media_icon"><a target="_blank" href="http://www.linkedin.com/company/tuco-marine-aps"><font-awesome-icon :icon="['fab', 'linkedin-in']"/></a></li>
+                </ul>
+              </div>
+          </b-col>
+          <b-col cols="12" class="map_location">
               <iframe frameborder="0" allowfullscreen
                 src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9128.103665226527!2d10.218865!3d55.112819!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x113b659355105862!2sTuco+Yacht+Shipyard+Ltd.!5e0!3m2!1sen!2sus!4v1550659258534" style="border:0" width="100%" height="100%">
               </iframe>
-            </div>
           </b-col>
         </b-row>
       </b-container>
