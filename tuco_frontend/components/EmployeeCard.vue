f7b9312a6e35d3c04e935157f133dd4900956008
Mathias Jacobsen <36068677+Mathias95@users.noreply.github.com>
2019-05-13 11:04:43 +0200

    added employee card

---
+++ tuco_frontend/components/EmployeeCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="employee_wrapper hover_bounce">
+        <div class="employee_overlay">
+            <div class="employee_info">
+                <h4 class="employee_name">Jonas Pedersen</h4>
+                <p class="employee_title">CEO &amp; Founder</p>
+                <a class="employee_mail" href="mailto:someone@example.com?Subject=Hello%20again"><font-awesome-icon :icon="['fas', 'envelope']"/>Send mail</a>
+            </div>
+        </div>
+        <img class="employee_img" src="~assets/img/Jonas_Pedersen.jpg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- tuco_frontend/pages/contact.vue
+++ tuco_frontend/pages/contact.vue
@@ -1,4 +1,5 @@
 <template>
+  <!--
   <div class id="contact">
     <div class>
       <div class="contact-box col-md-8">
@@ -26,156 +27,56 @@ DK-5600 Faaborg.
           </p>
         </div>
       </div>
-     <iframe class="bb col-md pr-0" frameborder="0" allowfullscreen
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9128.103665226527!2d10.218865!3d55.112819!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x113b659355105862!2sTuco+Yacht+Shipyard+Ltd.!5e0!3m2!1sen!2sus!4v1550659258534" width="600" height="450" style="border:0">
-      </iframe>
-    </div>
-    <div class="container employees-filter">
-      <div class="row py-5">
-        <div class="col-md-6 m-auto text-center">
-          <h1 class="boldtext h2 mt-4">THE PEOPLE BEHIND
-            <span class="colorgreentext">TUCO</span>
-          </h1>
-        </div>
-        <div class="emp-filtering mt-4 col-12 d-flex jc-center">
-          <div class="filter pb-3">
-            <a class="active" href="#all">Alle</a>
-            <a href="#Bestyrelse" class="Bestyrelse">Bestyrelsen</a>
-            <a href="#admin-commu" class="admin-commu">Administration og Kommunikation</a>
-            <a href="#management" class="management">Ledelse</a>
-            <a href="#it" class="press-contact">IT</a>
-            <a href="#subject-worker" class="subject-worker">Fagmedarbejdere</a>
-          </div>
-        </div>
-      </div>
-      <div class="employee-content row sets mb-5">
-        <div class="col-md-3">
-          <div class="employee-item Bestyrelse management">
-             <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
-            <div class="p-3">
-              <h2>Jonas Pedersen</h2>
-              <p>
-                <strong>Director</strong>
-              </p>
-              <a href="tel:xxxxxx">Mobil: xx xx xx xx</a><br>
-              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="employee-item Bestyrelse management">
-            <!-- <div
-              style="background-image: url('http://new.ecocouncil.dk/media/com_reditem/images/customfield/item/50/thumbs/800x800/060471a84a11b288711b173ec3d5235f6b28ebdc.jpg')"
-              class="img"></div> -->
-                <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
-            <div class="p-3">
-              <h2>Jonas Pedersen</h2>
-              <p>
-                <strong>Director</strong>
-              </p>
-              <a href="tel:xxxxxx">Mobil: xx xx xx xx</a><br>
-              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="employee-item Bestyrelse management">
-            <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
-            <div class="p-3">
-              <h2>Jonas Pedersen</h2>
-              <p>
-                <strong>Director</strong>
-              </p>
-              <a href="tel:xxxxxx">Mobil: xx xx xx xx</a><br>
-              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="employee-item Bestyrelse management">
-            <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
-            <div class="p-3">
-               <h2>Jonas Pedersen</h2>
-              <p>
-                <strong>Director</strong>
-              </p>
-              <a href="tel:xxxxxx">Mobil: xx xx xx </a><br>
-              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
-            </div>
-          </div>
-        </div>
-         <div class="col-md-3">
-          <div class="employee-item Bestyrelse management">
-            <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
-            <div class="p-3">
-               <h2>Jonas Pedersen</h2>
-              <p>
-                <strong>Director</strong>
-              </p>
-              <a href="tel:xxxxxx">Mobil: xx xx xx </a><br>
-              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
-            </div>
-          </div>
-        </div>
-         <div class="col-md-3">
-          <div class="employee-item Bestyrelse management">
-            <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
-            <div class="p-3">
-               <h2>Jonas Pedersen</h2>
-              <p>
-                <strong>Director</strong>
-              </p>
-              <a href="tel:xxxxxx">Mobil: xx xx xx </a><br>
-              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
-            </div>
-          </div>
-        </div>
-         <div class="col-md-3">
-          <div class="employee-item Bestyrelse management">
-            <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
-            <div class="p-3">
-               <h2>Jonas Pedersen</h2>
-              <p>
-                <strong>Director</strong>
-              </p>
-              <a href="tel:xxxxxx">Mobil: xx xx xx </a><br>
-              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
-            </div>
-          </div>
-        </div>
-         <div class="col-md-3">
-          <div class="employee-item Bestyrelse management">
-            <img src="../assets/img/Jonas_Pedersen.jpg" alt="">
-            <div class="p-3">
-               <h2>Jonas Pedersen</h2>
-              <p>
-                <strong>Director</strong>
-              </p>
-              <a href="tel:xxxxxx">Mobil: xx xx xx </a><br>
-              <a href="mailto:onas@tuco.dk">jonas@tuco.dk</a>
-            </div>
-          </div>
-        </div>
-      </div>
+     
     </div>
   </div>
+  -->
+
+  <div id="contact">
+    <subbanner/>
+    <section class="section_padding">
+      <b-container>
+        <b-row>
+          <b-col>
+            <div class="map_location">
+              <iframe frameborder="0" allowfullscreen
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9128.103665226527!2d10.218865!3d55.112819!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x113b659355105862!2sTuco+Yacht+Shipyard+Ltd.!5e0!3m2!1sen!2sus!4v1550659258534" style="border:0" width="100%" height="100%">
+              </iframe>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+    <section>
+      <b-container>
+        <b-row>
+          <b-col>
+            <h4>The people behind Tuco</h4>
+          </b-col>
+        </b-row>
+      </b-container>
+      <b-container>
+        <b-row>
+          <b-col cols="12" md="3" lg="4">
+            <EmployeeCard/>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+    <section class="section_padding">
+
+    </section>
+  </div>
 </template>
+
 <script>
+import subbanner from "~/components/subbanner.vue";
+import EmployeeCard from "~/components/EmployeeCard.vue";
+
 export default {
-  name: 'Contact'
+  components: {
+    subbanner,
+    EmployeeCard
+  }
 }
-//   ('.employees-filter .filter a').click(function(e) {
-//     e.preventDefault ()
-//     var a = (this).attr('href')
-//      a = a.substr (1)
-//      ('.sets .employee-item').each(function () {
-//      if (!(this).hasClass(a) && a !== 'all')
-//        (this).addClass('hide')
-//      else (this).removeClass('hide')
-//    })
-//  })
-//  ( ".filter a" ).on('click', function() {
-//       $(".filter a").removeClass('active')
-//       $(this).addClass('active')
-//     })
 </script>
