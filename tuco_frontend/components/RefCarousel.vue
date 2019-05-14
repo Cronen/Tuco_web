fc00a35bfce7707555b0b6ebeaab50ab070aca21
Thomas Crone <crone1992@gmail.com>
2019-05-14 19:37:40 +0200

    Set limit to 2 on carousel

--- tuco_frontend/components/RefCarousel.vue
+++ tuco_frontend/components/RefCarousel.vue
@@ -1,29 +1,40 @@
 <template>
-    <div>
-        <carousel>
-            <b-row>
-                <b-col class="review_wrapper">
-                    <div class="review_img">
-                        <img src="~assets/img/review2.jpg" alt="">
-                    </div>
-                    <div class="review_textwrapper">
-                        <h4 class="review_name">Jan Johansen</h4>
-                        <span class="review_company">IXblue</span>
-                        <p class="review_text">Lorem ipsum dolor sit amet, consectetur adiut labore et dolore magna aliqua. </p>
-                    </div>
-                </b-col>
-            </b-row>
-        </carousel>
-    </div>
+  <div>
+    <carousel items="2">
+      <div class="review_wrapper">
+        <div class="review_img">
+          <img src="~assets/img/review2.jpg" alt>
+        </div>
+        <div class="review_textwrapper">
+          <h4 class="review_name">Jan Johansen</h4>
+          <span class="review_company">IXblue</span>
+          <p
+            class="review_text"
+          >Lorem ipsum dolor sit amet, consectetur adiut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+       <div class="review_wrapper">
+        <div class="review_img">
+          <img src="~assets/img/review2.jpg" alt>
+        </div>
+        <div class="review_textwrapper">
+          <h4 class="review_name">Jan Johansen</h4>
+          <span class="review_company">IXblue</span>
+          <p
+            class="review_text"
+          >Lorem ipsum dolor sit amet, consectetur adiut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+    </carousel>
+  </div>
 </template>
 
 <script>
-    import carousel from 'vue-owl-carousel'
+//Read more about component here https://www.npmjs.com/package/vue-owl-carousel
+import carousel from "vue-owl-carousel";
 
-    export default {
-    components: { carousel },
-        
-    }
-
+export default {
+  components: { carousel }
+};
 </script>
 
