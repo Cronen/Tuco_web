468e63efd2a1b374b83ec42d85b80e0e8d6bae0d
Thomas Crone <crone1992@gmail.com>
2019-05-14 20:41:00 +0200

    Added modal for email sign-up. Not done yet

--- tuco_frontend/components/NewsletterForm.vue
+++ tuco_frontend/components/NewsletterForm.vue
@@ -9,14 +9,50 @@
               <p class="signup_subtext">Subscribe now and get the latest boat news</p>
               <div class="input_wrapper radius_box">
                 <form class="signup_form" action="">
-                  <input class="signup_input" type="email" name="email" placeholder="Enter your email adress">
-                  <button class="signup_btn" type="submit"><font-awesome-icon :icon="['fas', 'long-arrow-alt-right']"/></button>
+                  <input class="signup_input" type="email" name="email" v-model.trim="email" placeholder="Enter your email adress" >
+                  <button class="signup_btn" @click="open_modal"><font-awesome-icon :icon="['fas', 'long-arrow-alt-right']"/></button>
                 </form> 
               </div>
             </b-col>
           </b-row>
         </b-container>
       </div>
+        <b-modal id="mail_modal" >
+           <template slot="modal-header">
+            <font-awesome-icon :icon="['fas', 'times-circle']" class="close_icon" />
+            <img src="~assets/img/modal_header.png"/>
+          </template>
+          <template slot="default">
+            {{email}}
+          </template>
+          <template slot="modal-footer">
+            <b>Custom Footer</b>
+
+          </template>
+        </b-modal>
     </section>
 </template>
 
+<script>
+import emailModal from "~/components/emailModal.vue"
+
+export default {
+  data(){
+    return{
+      email: ''
+    }
+  },
+  components: {
+    emailModal
+  },
+  created(){
+    // console.log(this.email)
+  },
+  methods:{
+    open_modal(e){
+      e.preventDefault();
+      this.$bvModal.show('mail_modal');
+    }
+  }
+}
+</script>
---
+++ tuco_frontend/components/emailModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="color: black;">
+    {{email}}
+  </div>
+</template>
+
+<script>
+//Currently not in use, as I can't get it to work with passing props to modal in different component
+export default {
+  data() {
+    return {
+      email: this.mail
+    };
+  },
+  props: {
+    mail: String
+  }
+};
+</script>
