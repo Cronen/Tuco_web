12d3d6ef266a35a85af6b54059405f53a1ad4c6d
Pratheep <theep1891@gmail.com>
2019-05-14 13:06:37 +0200

    modified toggle component.

--- tuco_frontend/components/togglecollapsible.vue
+++ tuco_frontend/components/togglecollapsible.vue
@@ -1,5 +1,5 @@
 <template>
-     <div class="container">
+     <!-- <div class="container">
     <div class="panel-group" id="accordion">
     <div class="panel panel-default">
       <div class="panel-heading">
@@ -36,16 +36,45 @@
       </div>
     </div>
   </div> 
+</div> -->
+<div class="container">
+<div class="mb-4">
+  <b-text v-b-toggle.collapse-1 >OUR FUTURE</b-text>
+  <b-collapse id="collapse-1" class="mt-2">
+    <b-card bg-variant="transparent">
+      <p class="card-text"> One of the great strengths of Tuco Yacht Yard is the many different competences and that it makes it possible to have a very versatile production and several projects running at a time. Jonas Pedersen says, “That at Tuco they adapt to the needs of clients and to the market”. </p>
+    </b-card>
+  </b-collapse>
 </div>
+<div class="mb-4">
+  <b-text v-b-toggle.collapse-2 >OUR STRENGTH</b-text>
+  <b-collapse id="collapse-2" class="mt-2">
+    <b-card bg-variant="transparent">
+      <p class="card-text">Collapse contents Here</p>
+    </b-card>
+  </b-collapse>
+</div>
+<div class="mb-4">
+  <b-text v-b-toggle.collapse-3 >VISION</b-text>
+  <b-collapse id="collapse-3" class="mt-2">
+    <b-card bg-variant="transparent">
+      <p class="card-text">Collapse contents Here</p>
+    </b-card>
+  </b-collapse>
+</div>
+
+</div>
+
+
 </template>
 
 <script>
 export default {
-    head(){
-    return{
-     script: [{src:"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"}],
-     }
-  }
+//     head(){
+//     return{
+//      script: [{src:"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"}],
+//      }
+//   }
 }
 </script>
 
