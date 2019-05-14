d623400562e25fec516fa537d6ce4f94e024a9f7
Mathias Jacobsen <36068677+Mathias95@users.noreply.github.com>
2019-05-14 20:29:55 +0200

    case head styled needs changes on mobile

--- tuco_frontend/components/RefCarousel.vue
+++ tuco_frontend/components/RefCarousel.vue
@@ -1,29 +1,94 @@
 <template>
-    <div>
-        <carousel>
-            <b-row>
-                <b-col class="review_wrapper">
-                    <div class="review_img">
-                        <img src="~assets/img/review2.jpg" alt="">
-                    </div>
-                    <div class="review_textwrapper">
-                        <h4 class="review_name">Jan Johansen</h4>
-                        <span class="review_company">IXblue</span>
-                        <p class="review_text">Lorem ipsum dolor sit amet, consectetur adiut labore et dolore magna aliqua. </p>
-                    </div>
-                </b-col>
-            </b-row>
-        </carousel>
-    </div>
+  <div id="Items">
+    <carousel>
+      <b-col class="review_wrapper">
+        <div class="review_img">
+          <img src="~assets/img/review2.jpg" alt>
+        </div>
+        <div class="review_textwrapper">
+          <h4 class="review_name">Jan Johansen</h4>
+          <span class="review_company">IXblue</span>
+          <p
+            class="review_text"
+          >Lorem ipsum dolor sit amet, consectetur adiut labore et dolore magna aliqua.</p>
+        </div>
+      </b-col>
+      <b-col class="review_wrapper item">
+        <div class="review_img">
+          <img src="~assets/img/review2.jpg" alt>
+        </div>
+        <div class="review_textwrapper item">
+          <h4 class="review_name">Jan Johansen</h4>
+          <span class="review_company">IXblue</span>
+          <p
+            class="review_text"
+          >Lorem ipsum dolor sit amet, consectetur adiut labore et dolore magna aliqua.</p>
+        </div>
+      </b-col>
+      <b-col class="review_wrapper item">
+        <div class="review_img">
+          <img src="~assets/img/review2.jpg" alt>
+        </div>
+        <div class="review_textwrapper item">
+          <h4 class="review_name">Jan Johansen</h4>
+          <span class="review_company">IXblue</span>
+          <p
+            class="review_text"
+          >Lorem ipsum dolor sit amet, consectetur adiut labore et dolore magna aliqua.</p>
+        </div>
+      </b-col>
+      <b-col class="review_wrapper">
+        <div class="review_img">
+          <img src="~assets/img/review2.jpg" alt>
+        </div>
+        <div class="review_textwrapper">
+          <h4 class="review_name">Jan Johansen</h4>
+          <span class="review_company">IXblue</span>
+          <p
+            class="review_text"
+          >Lorem ipsum dolor sit amet, consectetur adiut labore et dolore magna aliqua.</p>
+        </div>
+      </b-col>
+    </carousel>
+  </div>
 </template>
 
 <script>
-    import carousel from 'vue-owl-carousel'
+import carousel from "vue-owl-carousel";
 
-    export default {
-    components: { carousel },
-        
+export default {
+  components: {
+    carousel
+  },
+  method: {
+    installOwlCarousel: function() {
+      $('.owl-carousel').owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 900,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 2,
+                nav: true,
+                loop: false,
+                margin: 20
+            }
+        }
+      });
     }
-
+  }
+};
 </script>
 
