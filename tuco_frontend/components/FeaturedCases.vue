cf0a3b448dd3148133e8d5e82ada4f856ac986a8
Thomas Crone <crone1992@gmail.com>
2019-05-15 14:36:45 +0200

    Finished FeaturedCases component for frontpage

--- tuco_frontend/components/FeaturedCases.vue
+++ tuco_frontend/components/FeaturedCases.vue
@@ -1,22 +1,56 @@
 <template>
-    <section>
-      <b-container>
-        <b-row>
-          <b-col>
-            <h5 class="left_border">Latest project cases</h5>
-            <div v-if="true">
-              Reference name from Strapi:
-              <!--<b>{{references[0].ref_name}}</b>-->
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
-    </section>
+  <section>
+    <b-container>
+      <b-row>
+        <b-col>
+          <h5 class="left_border">Latest project cases</h5>
+        </b-col>
+      </b-row>
+      <b-row v-if="!loading" class="my-3">
+        <b-col cols="12" md="6" lg="4" class="mb-30" v-for="c in cases" v-bind:key="c.id">
+          <CaseCard :case="c"/>
+        </b-col>
+      </b-row>
+      <b-row v-else>
+        <b-col cols="1" class="mx-auto">
+          <Loader/>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
 </template>
 
 <script>
-export default {
+import CaseCard from "~/components/CaseCard.vue";
+import Loader from "~/components/Loader.vue";
+import Strapi from "strapi-sdk-javascript";
+const strapi_url = process.env.StrapiUrl;
+const strapi = new Strapi(strapi_url);
 
-}
+export default {
+  data() {
+    return {
+      cases: null,
+      loading: false
+    };
+  },
+  components: {
+    Loader,
+    CaseCard
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      this.loading = true;
+      let response = await strapi
+        .request("get", "/cases?_sort=createdAt:DESC&featured=true&_limit=3")
+        .then((this.loading = false));
+      console.log(response)
+      this.cases = response
+    }
+  }
+};
 </script>
 
