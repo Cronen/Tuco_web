c1a18364dc8af41200c51a72696734edcd41355b
Mathias Jacobsen <36068677+Mathias95@users.noreply.github.com>
2019-05-16 01:29:13 +0200

    added story section frontpage needs styling

--- tuco_frontend/components/IntroAbout.vue
+++ tuco_frontend/components/IntroAbout.vue
@@ -1,23 +1,41 @@
 <template>
+  <div>
     <section class="intro_section">
       <b-container>
         <b-row class="justify-content-center">
           <b-col cols="12" md="10" lg="8">
             <h2 class="intro_title">Classification Projects</h2>
-            <p class="intro_subtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p
+              class="intro_subtext"
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           </b-col>
         </b-row>
       </b-container>
+    </section>
+    <section class="story_section">
       <b-container>
         <b-row>
-
+          <b-col>
+            <div class="story_wrapper">
+              <h2 class="story_title">Tuco Marine Group</h2>
+              <div class="story_text">
+                <p class="mb-20">At Tuco Marine Group we specialize in light structures and hulls manufactured with composite materials such as carbon fibre. Tuco supplies a range of different composite structures for the shipping industry and other industries where low weight and strength are important factors.</p>
+                <p>In 1998 when the company was founded it was a traditional yard and the main business area was sailing boats. Today focus has shifted to the construction of large structures primarily in the commercial maritime sector.</p>
+              </div>
+              <a href="#">
+                Full story
+                <font-awesome-icon :icon="['fas', 'long-arrow-alt-right']"/>
+              </a>
+            </div>
+          </b-col>
+          <img src="~assets/img/story-img1.jpg" alt="">
+          <img src="~assets/img/story-img2.jpg" alt="">
         </b-row>
       </b-container>
     </section>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
