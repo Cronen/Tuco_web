6600282651ee08528760183129d5c38286ad6baa
Pratheep <theep1891@gmail.com>
2019-05-21 14:06:38 +0200

    front boat - problem to get featured.

--- tuco_frontend/components/FrontProduct.vue
+++ tuco_frontend/components/FrontProduct.vue
@@ -3,26 +3,26 @@
         <b-container>
             <b-row v-for="b in this.boats" :key="b.id">
                 <b-col cols="12" md="5" class="product_wrapper">
-                    <h5 class="product_title">{{b.Boat_name}}</h5>
-                    <p class="product_description">{{b.Boat_description}}</p>
+                    <h5 class="product_title">{{b.boat_name}}</h5>
+                    <p class="product_description">{{b.boat_description}}</p>
                     <ul class="product_detail">
                         <li> 
                             <div class="product_icon"><img src="~assets/img/product-img1.png" alt=""></div>
-                            <span>Length:</span>{{b.Boat_length}}
+                            <span>Length:</span>{{b.boat_length}}
                         </li>
                         <li> 
                             <div class="product_icon"><img src="~assets/img/product-img2.png" alt=""></div>
-                            <span>Beam:</span>{{b.Boat_beam}}
+                            <span>Beam:</span>{{b.boat_beam}}
                         </li>
                         <li> 
                             <div class="product_icon"><img src="~assets/img/product-img3.png" alt=""></div>
-                            <span>Engine:</span>{{b.Boat_engine}}
+                            <span>Engine:</span>{{b.boat_engine}}
                         </li>
                     </ul>
                     <a href="" class="btn btn-main">View boat</a>
                 </b-col>
                 <b-col cols="12" md="7" class="product_img_wrapper">
-                    <img src="#" alt="">
+                    <img src="" alt="">
                 </b-col>
             </b-row>
         </b-container>
@@ -31,7 +31,6 @@
 
 <script>
 import Strapi from "strapi-sdk-javascript";
-//import Loader from "~/components/Loader.vue";
 const strapi_url = process.env.StrapiUrl;
 const strapi = new Strapi(strapi_url);
 export default {
@@ -41,10 +40,7 @@ export default {
       loading: true
     
     };
-//   },
-//    components: {
-//     Loader
-   
+
   },
    created() {
     this.fetchData();
@@ -53,11 +49,15 @@ export default {
     async fetchData() {
       this.loading = true;
       let response = await strapi
-        .request("get", "/boats")
+        .request("get", "/boats?_&_boat_featured=true&_limit=1")
         .then((this.loading = false));
       console.log(response);
       this.boats = response;
-    }
+    },
+    // get_image_url(boats){
+    //     return strapi_url + boats.boat_image.url;
+    // },
+
 
 }
 };
