33a1eae603c8814d9ac816f5dcff9095b255f0ca
Pratheep <theep1891@gmail.com>
2019-05-21 12:05:47 +0200

    Boats Front-end  changed

--- tuco_frontend/components/FrontProduct.vue
+++ tuco_frontend/components/FrontProduct.vue
@@ -1,28 +1,28 @@
 <template>
     <section class="section_padding">
         <b-container>
-            <b-row>
+            <b-row v-for="b in this.boats" :key="b.id">
                 <b-col cols="12" md="5" class="product_wrapper">
-                    <h5 class="product_title">Prozero 10.5m wbw archipilargo</h5>
-                    <p class="product_description">The unique ProZero system with a flexible attached cabin reducing noise from engines.</p>
+                    <h5 class="product_title">{{b.Boat_name}}</h5>
+                    <p class="product_description">{{b.Boat_description}}</p>
                     <ul class="product_detail">
                         <li> 
                             <div class="product_icon"><img src="~assets/img/product-img1.png" alt=""></div>
-                            <span>Length:</span>10.5m
+                            <span>Length:</span>{{b.Boat_length}}
                         </li>
                         <li> 
                             <div class="product_icon"><img src="~assets/img/product-img2.png" alt=""></div>
-                            <span>Beam:</span>3.5m
+                            <span>Beam:</span>{{b.Boat_beam}}
                         </li>
                         <li> 
                             <div class="product_icon"><img src="~assets/img/product-img3.png" alt=""></div>
-                            <span>Engine:</span>1 x Diesel
+                            <span>Engine:</span>{{b.Boat_engine}}
                         </li>
                     </ul>
-                    <a href="#" class="btn btn-main">View boat</a>
+                    <a href="" class="btn btn-main">View boat</a>
                 </b-col>
                 <b-col cols="12" md="7" class="product_img_wrapper">
-                    <img src="~assets/img/productmain-img.png" alt="">
+                    <img src="#" alt="">
                 </b-col>
             </b-row>
         </b-container>
@@ -30,7 +30,35 @@
 </template>
 
 <script>
+import Strapi from "strapi-sdk-javascript";
+//import Loader from "~/components/Loader.vue";
+const strapi_url = process.env.StrapiUrl;
+const strapi = new Strapi(strapi_url);
 export default {
+    data() {
+    return {
+      boats: null,
+      loading: true
+    
+    };
+//   },
+//    components: {
+//     Loader
+   
+  },
+   created() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      this.loading = true;
+      let response = await strapi
+        .request("get", "/boats")
+        .then((this.loading = false));
+      console.log(response[5]);
+      this.boats = response;
+    }
 
 }
+};
 </script>
