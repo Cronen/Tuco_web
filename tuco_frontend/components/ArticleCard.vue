d2ac0afb475998b05391c7e5c0e67c796d90dcd0
Thomas Crone <crone1992@gmail.com>
2019-06-01 21:18:40 +0200

    Finished the news page with data from Strapi -Needs fixing with text overflow: eclipse

--- tuco_frontend/components/ArticleCard.vue
+++ tuco_frontend/components/ArticleCard.vue
@@ -1,57 +1,93 @@
 <template>
-<div id="articleList">
+  <!-- <div id="articleList">
     <b-row>
-        <b-col xs="12" md="5" lg="6" class="article_imgwrapper">
-            <img src="~assets/img/0b48acbe30999524ca3a49913345b1e9.jpg" alt="">
-        </b-col>
-        <b-col xs="12" md="7" lg="6" class="article_textwrapper">
-            <div class="article_inner">
-                <span class="article_label date_label">News post</span> 
-                <div class="article_meta">
-                    <div class="article_timestamp">
-                        <span><time>March 8 2019 13.49 pm</time></span>
-                    </div>
-                    <ul class="article_share">
-                        <li class="article_icon"><a target="_blank" href="https://www.facebook.com/pages/Tuco-Marine-Group-ApS/250239778331751?fref=ts"><font-awesome-icon :icon="['fab', 'facebook-f']"/></a></li>
-                        <li class="article_icon"><a target="_blank" href="http://www.linkedin.com/company/tuco-marine-aps"><font-awesome-icon :icon="['fab', 'linkedin-in']"/></a></li>
-                        <li class="article_icon"><a target="_blank" href="http://www.youtube.com/tucomarine"><font-awesome-icon :icon="['fas', 'envelope']"/></a></li>
-                    </ul>
-                </div>
-                <h2 class="article_title">
-                    <a href="#">Eleanor - 12m demonstrator for sale.</a>
-                </h2>
-                <p class="article_introtext">Our 12m light pilot classed “Eleanor” is now offered for sale after a successful charter. Full specifications, General Arrangement as well as asking price can be obtained by contacting our team as...</p>
+      <b-col xs="12" md="5" lg="6" class="article_imgwrapper">
+        <img src="~assets/img/0b48acbe30999524ca3a49913345b1e9.jpg" alt>
+      </b-col>
+      <b-col xs="12" md="7" lg="6" class="article_textwrapper">
+        <div class="article_inner">
+          <span class="article_label date_label">News post</span>
+          <div class="article_meta">
+            <div class="article_timestamp">
+              <span>
+                <time>March 8 2019 13.49 pm</time>
+              </span>
             </div>
-        </b-col>
+            <ul class="article_share">
+              <li class="article_icon">
+                <a
+                  target="_blank"
+                  href="https://www.facebook.com/pages/Tuco-Marine-Group-ApS/250239778331751?fref=ts"
+                >
+                  <font-awesome-icon :icon="['fab', 'facebook-f']"/>
+                </a>
+              </li>
+              <li class="article_icon">
+                <a target="_blank" href="http://www.linkedin.com/company/tuco-marine-aps">
+                  <font-awesome-icon :icon="['fab', 'linkedin-in']"/>
+                </a>
+              </li>
+              <li class="article_icon">
+                <a target="_blank" href="http://www.youtube.com/tucomarine">
+                  <font-awesome-icon :icon="['fas', 'envelope']"/>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <h2 class="article_title">
+            <a href="#">Eleanor - 12m demonstrator for sale.</a>
+          </h2>
+          <p
+            class="article_introtext"
+          >Our 12m light pilot classed “Eleanor” is now offered for sale after a successful charter. Full specifications, General Arrangement as well as asking price can be obtained by contacting our team as...</p>
+        </div>
+      </b-col>
     </b-row>
-</div>
+  </div>-->
 
-<!-- <div id="articleList" v-if="!loading">
-    <b-row v-for="n in this.news" :key="n.id">
-        <b-col xs="12" md="5" lg="6" class="article_imgwrapper">
-            <img :src="get_image_url(n.news_image.url)" alt="">
-        </b-col>
-        <b-col xs="12" md="7" lg="6" class="article_textwrapper">
-            <div class="article_inner">
-                <span class="article_label date_label">News post</span> 
-                <div class="article_meta">
-                    <div class="article_timestamp">
-                        <span><time>{{n.news_time}}</time></span>
-                    </div>
-                    <ul class="article_share">
-                        <li class="article_icon"><a target="_blank" href="https://www.facebook.com/pages/Tuco-Marine-Group-ApS/250239778331751?fref=ts"><font-awesome-icon :icon="['fab', 'facebook-f']"/></a></li>
-                        <li class="article_icon"><a target="_blank" href="http://www.linkedin.com/company/tuco-marine-aps"><font-awesome-icon :icon="['fab', 'linkedin-in']"/></a></li>
-                        <li class="article_icon"><a target="_blank" href="http://www.youtube.com/tucomarine"><font-awesome-icon :icon="['fas', 'envelope']"/></a></li>
-                    </ul>
-                </div>
-                <h2 class="article_title">
-                    <a :href="n.news_url" target="_blank">{{n.news_title}}</a>
-                </h2>
-                <p class="article_introtext">{{n.news_description}}</p>
+  <div id="articleList">
+    <b-row>
+      <b-col xs="12" md="5" lg="6" class="article_imgwrapper">
+        <img :src="img_url" alt>
+      </b-col>
+      <b-col xs="12" md="7" lg="6" class="article_textwrapper">
+        <div class="article_inner">
+          <span class="article_label date_label">News post</span>
+          <div class="article_meta">
+            <div class="article_timestamp">
+              <span>
+                <time>{{date}}</time>
+              </span>
             </div>
-        </b-col>
+            <ul class="article_share">
+              <li class="article_icon">
+                <a
+                  target="_blank"
+                  href="https://www.facebook.com/pages/Tuco-Marine-Group-ApS/250239778331751?fref=ts"
+                >
+                  <font-awesome-icon :icon="['fab', 'facebook-f']"/>
+                </a>
+              </li>
+              <li class="article_icon">
+                <a target="_blank" href="http://www.linkedin.com/company/tuco-marine-aps">
+                  <font-awesome-icon :icon="['fab', 'linkedin-in']"/>
+                </a>
+              </li>
+              <li class="article_icon">
+                <a target="_blank" href="http://www.youtube.com/tucomarine">
+                  <font-awesome-icon :icon="['fas', 'envelope']"/>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <h2 class="article_title">
+            <a :href="link_url" target="_blank">{{title}}</a>
+          </h2>
+          <p class="article_introtext">{{intro_txt}}</p>
+        </div>
+      </b-col>
     </b-row>
-</div> -->
+  </div>
 </template>
 
 <script>
@@ -60,29 +96,12 @@ const strapi_url = process.env.StrapiUrl;
 const strapi = new Strapi(strapi_url);
 
 export default {
-     data() {
-    return {
-    news: null,
-    loading: true
-  };
-  },
-    created() {
-    this.fetchData();
-    console.log("news", this.news);
-
-  },
-  methods: {
-    async fetchData() {
-      this.loading = true;
-      let response = await strapi
-        .request("get", "/news")
-        .then((this.loading = false));
-      console.log("news", response);
-      this.news = response;
-    },
-    get_image_url(url){
-        return strapi_url + url;
-    },
- }
-}
+  props: {
+    img_url: String,
+    date: String,
+    title: String,
+    intro_txt: String,
+    link_url: String
+  }
+};
 </script>
--- tuco_frontend/pages/news/index.vue
+++ tuco_frontend/pages/news/index.vue
@@ -1,34 +1,104 @@
 <template>
-    <div id="newsList">
-        <subbanner title="News"/>
-        <section class="section_padding">
-            <b-container>
-                <b-row>
-                    <b-col md="7" lg="6" class="mb-80">
-                        <h2 class="subpage_title">News Articles</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
-                    </b-col>
-                </b-row>
-            </b-container>
-            <b-container>
-                <b-row>
-                    <b-col>
-                        <ArticleCard/>
-                    </b-col>
-                </b-row>
-            </b-container>
-        </section>   
-    </div> 
+  <div id="newsList">
+    <subbanner title="News"/>
+    <section class="section_padding">
+      <b-container>
+        <b-row>
+          <b-col md="7" lg="6" class="mb-80">
+            <h2 class="subpage_title">News Articles</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+          </b-col>
+        </b-row>
+      </b-container>
+      <b-container>
+        <b-row v-if="!loading">
+          <b-col sm="12" class="mb-4" > 
+              <!-- This as test -->
+            <ArticleCard
+              title="Eleanor - 12m demonstrator for sale."
+              intro_txt="Our 12m light pilot classed “Eleanor” is now offered for sale after a successful charter. Full specifications, General Arrangement as well as asking price can be obtained by contacting our team as..."
+              date="March 8 2019 13.49"
+              img_url="/_nuxt/assets/img/0b48acbe30999524ca3a49913345b1e9.jpg"
+              link_url="#"
+            />
+          </b-col>
+          <b-col sm="12" class="mb-4" v-for="n in news" :key="n.id">
+            <ArticleCard
+              :title="n.news_title"
+              :intro_txt="n.news_description"
+              :date="get_date(n.news_time)"
+              :img_url="get_image_url(n.News_image.url)"
+              :link_url="n.News_url"
+            />
+          </b-col>
+        </b-row>
+        <b-row v-else>
+          <b-col cols="1" class="mx-auto">
+            <Loader/>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+  </div>
 </template>
 
 <script>
 import subbanner from "~/components/subbanner.vue";
+import Loader from "~/components/Loader.vue";
 import ArticleCard from "~/components/ArticleCard.vue";
+import Strapi from "strapi-sdk-javascript";
+const strapi_url = process.env.StrapiUrl;
+const strapi = new Strapi(strapi_url);
 
 export default {
-    components: {
-        subbanner,
-        ArticleCard
+  data() {
+    return {
+      news: null,
+      loading: true
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  components: {
+    subbanner,
+    ArticleCard,
+    Loader
+  },
+  methods: {
+    async fetchData() {
+      this.loading = true;
+      let response = await strapi
+        .request("get", "/news")
+        .then((this.loading = false));
+      console.log("news", response);
+      this.news = response;
+    },
+    get_image_url(url) {
+      return strapi_url + url;
+    },
+    get_date(date) {
+      let d = new Date(date);
+      var months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      let mins = ("0" + d.getMinutes()).slice(-2);
+      let hours = ("0" + d.getUTCHours()).slice(-2);
+      return `${
+        months[d.getMonth()]
+      } ${d.getDate()} ${d.getFullYear()} ${hours}.${mins}`;
     }
-}
+  }
+};
 </script>
